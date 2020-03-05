228a5d548c13fe1b9d8ce3da4c6e6fabc7e3876a
srjgrg <srjlamichane@gmail.com>
2020-03-05 17:28:55 -0500

    build

---
+++ build/precache-manifest.a49a8957c62830b6b5a90cd99dd2fd7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8408b82abbaeac8efd0d35f9f82f4ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e42bddc1b3b7f2d23ad",
+    "url": "/static/js/2.416dc709.chunk.js"
+  },
+  {
+    "revision": "a12b0ad61ed1bd03ed0a2b764c8a6c2a",
+    "url": "/static/js/2.416dc709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47fc33ffd091c094ac79",
+    "url": "/static/js/main.20b6c95e.chunk.js"
+  },
+  {
+    "revision": "ff455a35fff707e9bbd9",
+    "url": "/static/js/runtime-main.4e190b96.js"
+  }
+]);
